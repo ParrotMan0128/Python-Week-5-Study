e8ae1f8c0744c5acb449160aa26337ea2a73b7ae
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-04-06 17:10:47 +0900

    Update 20233343_Problem2.py

--- 20233343_Problem2.py
+++ 20233343_Problem2.py
@@ -1,18 +1,15 @@
 fruits = ["사과", "감귤"];
-passCount = 0;
 
-while (len(fruits) < 5):
+while (len(fruits) <5 ):
 
-    s = input("저장할 과일을 입력하시오.\n>> ");
+  fruit = input("어떤 과일을 저장할까요?>>> ");
 
-    if (s in fruits):
+  if (fruit in fruits):
 
-        print("이미 동일한 과일이 저장되어있습니다.");
+    print("동일한 과일이 있습니다.");
+    continue;
 
-    else:
-
-        fruits.append(s);
-        print("입력이 " + str(5 - len(fruits)) + "번 남았습니다.");
-
-print("5개 과일은 " + str(fruits) + " 입니다.");
-
+  fruits.append(fruit);
+  print("입력이 " + str(5 - len(fruits)) + "번 남았습니다.");
+  
+print("5개과일은 " + str(fruits) + "입니다");
